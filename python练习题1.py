774885bdb6911ca1e28891f968d6e534c05d7cca
YanKai <yankai_hw@163.com>
2020-04-17 18:27:29 +0800

    接口复习完成day2

---
+++ python练习题1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# enccoding: utf-8
+'''
+@author: Yankai
+@project:
+'''
+class cat(object):
+
+    def eat(self):
+        print('小猫爱吃鱼')
+    def drink(self):
+        print('小猫爱喝水')
+a = cat()
+a.eat()
+a.drink()
